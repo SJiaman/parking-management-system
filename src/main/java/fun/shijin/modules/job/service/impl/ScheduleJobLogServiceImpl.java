dc3129bf7d46a5f5af63d3fb46a284315cc66692
jiaman <2433313140@qq.com>
2022-03-28 16:30:10 +0800

    init project

---
+++ src/main/java/fun/shijin/modules/job/service/impl/ScheduleJobLogServiceImpl.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2016-2019 人人开源 All rights reserved.
+ *
+ * https://www.renren.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package fun.shijin.modules.job.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import fun.shijin.modules.job.entity.ScheduleJobLogEntity;
+import fun.shijin.common.utils.PageUtils;
+import fun.shijin.common.utils.Query;
+import fun.shijin.modules.job.dao.ScheduleJobLogDao;
+import fun.shijin.modules.job.service.ScheduleJobLogService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service("scheduleJobLogService")
+public class ScheduleJobLogServiceImpl extends ServiceImpl<ScheduleJobLogDao, ScheduleJobLogEntity> implements ScheduleJobLogService {
+
+	@Override
+	public PageUtils queryPage(Map<String, Object> params) {
+		String jobId = (String)params.get("jobId");
+
+		IPage<ScheduleJobLogEntity> page = this.page(
+			new Query<ScheduleJobLogEntity>().getPage(params),
+			new QueryWrapper<ScheduleJobLogEntity>().like(StringUtils.isNotBlank(jobId),"job_id", jobId)
+		);
+
+		return new PageUtils(page);
+	}
+
+}
---
+++ src/main/java/fun/shijin/modules/oss/service/impl/SysOssServiceImpl.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2016-2019 人人开源 All rights reserved.
+ *
+ * https://www.renren.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package fun.shijin.modules.oss.service.impl;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import fun.shijin.common.utils.PageUtils;
+import fun.shijin.common.utils.Query;
+import fun.shijin.modules.oss.dao.SysOssDao;
+import fun.shijin.modules.oss.entity.SysOssEntity;
+import fun.shijin.modules.oss.service.SysOssService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+
+@Service("sysOssService")
+public class SysOssServiceImpl extends ServiceImpl<SysOssDao, SysOssEntity> implements SysOssService {
+
+	@Override
+	public PageUtils queryPage(Map<String, Object> params) {
+		IPage<SysOssEntity> page = this.page(
+			new Query<SysOssEntity>().getPage(params)
+		);
+
+		return new PageUtils(page);
+	}
+	
+}
